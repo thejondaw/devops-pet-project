2e7a1548fe7ad53567716156854aa516acdbf100
thejondaw <mrjondaw@gmail.com>
2025-01-06 12:28:02 -0600

    01/06/25

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -2,6 +2,27 @@
 # ================== BACKEND MODULE ================== #
 # ==================================================== #
 
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "alexsuff"
+    key     = "project/backend.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+  }
+}
+
+# Provider configuration using variables
+provider "aws" {
+  region = var.region
+}
+
 # S3 Bucket - Terraform State
 data "aws_s3_bucket" "terraform_state" {
   bucket = var.backend_bucket
