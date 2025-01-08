eb39f6e3645acdda242ea8d609e18dc9a90166d8
thejondaw <mrjondaw@gmail.com>
2025-01-08 14:20:39 -0600

    01/08/25

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -187,9 +187,7 @@ resource "aws_iam_role_policy_attachment" "node_group_minimum_policies" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
-    "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   ])
 
   policy_arn = each.value
--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -2,6 +2,8 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_configuration.cidr
   instance_tenancy = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name        = "devops-project-vpc"
