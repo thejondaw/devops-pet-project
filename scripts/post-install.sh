a9bd67a65d325feb0439daf519f893df9e73bc3d
thejondaw <mrjondaw@gmail.com>
2025-01-08 18:17:46 -0600

    01/08/25

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Get environment from first argument or use develop as default
+ENVIRONMENT=${1:-develop}
+
 # Connecting to EKS Cluster
-CLUSTER_NAME=$(aws eks list-clusters --region us-east-2 --query "clusters[?contains(@, 'develop')]|[0]" --output text)
+CLUSTER_NAME=$(aws eks list-clusters --region us-east-2 --query "clusters[?contains(@, '${ENVIRONMENT}')]|[0]" --output text)
 if [ -z "$CLUSTER_NAME" ]; then
-  echo "Error: Cluster not found!"
+  echo "Error: Cluster not found for environment ${ENVIRONMENT}!"
   exit 1
 fi
 aws eks update-kubeconfig --name $CLUSTER_NAME --region us-east-2
@@ -19,11 +22,12 @@ cd helm/charts/monitoring && helm dependency build && cd ../../..
 cd helm/charts/vault && helm dependency build && cd ../../..
 
 # Install Applications via ArgoCD
-kubectl apply -f k8s/argocd/applications/master/metrics-server.yaml
-kubectl apply -f k8s/argocd/applications/master/aws-ebs-csi-driver.yaml
-kubectl apply -f k8s/argocd/applications/master/ingress-nginx.yaml
-kubectl apply -f k8s/argocd/applications/master/monitoring.yaml
-kubectl apply -f k8s/argocd/applications/master/vault.yaml
+kubectl apply -f k8s/argocd/applications/${ENVIRONMENT}/metrics-server.yaml
+kubectl apply -f k8s/argocd/applications/${ENVIRONMENT}/aws-ebs-csi-driver.yaml
+kubectl apply -f k8s/argocd/applications/${ENVIRONMENT}/ingress-nginx.yaml
+kubectl apply -f k8s/argocd/applications/${ENVIRONMENT}/monitoring.yaml
+kubectl apply -f k8s/argocd/applications/${ENVIRONMENT}/vault.yaml
 
-# kubectl apply -f k8s/argocd/applications/master/api.yaml
-# kubectl apply -f k8s/argocd/applications/master/web.yaml
+# Commented out for now
+# kubectl apply -f k8s/argocd/applications/${ENVIRONMENT}/api.yaml
+# kubectl apply -f k8s/argocd/applications/${ENVIRONMENT}/web.yaml
