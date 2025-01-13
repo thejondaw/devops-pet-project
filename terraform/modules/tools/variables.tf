6aaa88a83dd7eb2209aa76c5c37b64a52358a723
thejondaw <mrjondaw@gmail.com>
2025-01-13 14:05:26 -0600

    01/13/25

--- terraform/modules/tools/variables.tf
+++ terraform/modules/tools/variables.tf
@@ -27,20 +27,21 @@ variable "argocd_server_service" {
   }
 }
 
-# variable "argocd_server_service" {
-#   description = "ArgoCD server service configuration"
-#   type = object({
-#     type                 = string
-#     load_balancer_type   = string
-#     cross_zone_enabled   = bool
-#     load_balancer_scheme = string
-#     source_ranges        = list(string)
-#   })
-#   default = {
-#     type                 = "LoadBalancer"
-#     load_balancer_type   = "nlb"
-#     cross_zone_enabled   = true
-#     load_balancer_scheme = "internet-facing"
-#     source_ranges        = ["0.0.0.0/0"]
-#   }
-# }
+# Variable - ArgoCD Service
+variable "argocd_server_service" {
+  description = "ArgoCD server service configuration"
+  type = object({
+    type                 = string
+    load_balancer_type   = string
+    cross_zone_enabled   = bool
+    load_balancer_scheme = string
+    source_ranges        = list(string)
+  })
+  default = {
+    type                 = "LoadBalancer"
+    load_balancer_type   = "nlb"
+    cross_zone_enabled   = true
+    load_balancer_scheme = "internet-facing"
+    source_ranges        = ["0.0.0.0/0"]
+  }
+}
